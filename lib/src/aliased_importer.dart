81d5dc4a2559bb1dc61e5d9a34c2af4968f5c9ca
qucumbah <39967396+qucumbah@users.noreply.github.com>
2024-11-07 08:43:08 +0300

    add header

--- lib/src/aliased_importer.dart
+++ lib/src/aliased_importer.dart
@@ -1,3 +1,9 @@
+// Copyright 2019 Google LLC
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 import 'package:path/path.dart' as p;
 
 import 'package:sass_api/sass_api.dart';
